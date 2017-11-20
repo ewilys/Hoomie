22fcd9a73faf98aa86077d5bd3bbd9876dc912f9
Guillaume Ferron <guillaumef.22@gmail.com>
2017-11-20 22:58:18 +0100

    Refactoring of temperatureView and temperatureContainer
    Now it is called CHartView and ChartContainer
    It uses the utils methods to display a chart

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, {Component} from 'react';
 import styled from 'styled-components/native';
-import TemperatureContainer from "../containers/TemperatureContainer";
+import ChartContainer from "../containers/ChartContainer";
 
 const ContainerView = styled.View`
   flex: 1;
@@ -13,7 +13,7 @@ class HomeScreen extends Component {
     render() {
         return (
             <ContainerView>
-                <TemperatureContainer/>
+                <ChartContainer/>
             </ContainerView>
         );
     }
