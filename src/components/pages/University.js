23f9efb723a6b5cb0b7fd23e17eaab782cc523f1
Guillaume Ferron <guillaumef.22@gmail.com>
2017-12-13 17:44:54 +0100

    Picker for the room, university side

--- src/components/pages/University.js
+++ src/components/pages/University.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React, {Component} from 'react';
 import TemperatureChartContainer from "../../containers/ui/TemperatureChartContainer";
-import {RefreshControl, ScrollView} from "react-native";
+import {Picker, RefreshControl, ScrollView} from "react-native";
 import {screenStyle} from "../../utils/constants";
+import {getCurrentMonthAsStr, getCurrentYear} from "../../utils/methods";
 
 class University extends Component {
     constructor() {
@@ -25,8 +26,12 @@ class University extends Component {
     render() {
         return (
             <ScrollView style={screenStyle} refreshControl={<RefreshControl refreshing={this.state.isRefreshing} onRefresh={this._onRefresh.bind(this)} />}>
-                <TemperatureChartContainer room={this.state.room} subparameters={{period: "year"}} homeRefreshing={this.state.isRefreshing} homeRefreshed={this.hasRefreshed.bind(this)}/>
-                <TemperatureChartContainer room={this.state.room} subparameters={{period: "month"}} homeRefreshing={this.state.isRefreshing} homeRefreshed={this.hasRefreshed.bind(this)}/>
+                <Picker style={{width: 200}} itemStyle={{fontSize: 15, fontWeight: 'bold'}} selectedValue={this.state.room} onValueChange={(itemValue, itemIndex) => this.setState({room: itemValue})} mode='dropdown'>
+                    <Picker.Item label="Room 204" value="204" />
+                    <Picker.Item label="Room 205" value="205" />
+                </Picker>
+                <TemperatureChartContainer room={this.state.room} chartTitle={getCurrentYear() + " Temperatures"} subparameters={{period: "year"}} homeRefreshing={this.state.isRefreshing} homeRefreshed={this.hasRefreshed.bind(this)}/>
+                <TemperatureChartContainer room={this.state.room} chartTitle={getCurrentMonthAsStr() + " Temperatures"} subparameters={{period: "month"}} homeRefreshing={this.state.isRefreshing} homeRefreshed={this.hasRefreshed.bind(this)}/>
             </ScrollView>
         );
     }
