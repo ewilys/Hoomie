258262fc2e436e1f9ea51af845c24952ceb18ac8
Guillaume Ferron <guillaumef.22@gmail.com>
2017-12-13 23:45:38 +0100

    Use of the advice component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,15 @@
 import React, {Component} from 'react';
 import {ScrollView} from "react-native";
 import {screenStyle} from "../../utils/constants";
+import Advice from "../ui/Advice";
 
 class HomeScreen extends Component {
 
     render() {
         return (
             <ScrollView style={screenStyle}>
+                <Advice content="Do you know that if you lower your heater temperature by 1°C during the night, you'll save 7% on your energy bill ?"/>
+                <Advice content="Hey ! You forgot to close your window today !"/>
             </ScrollView>
         );
     }
