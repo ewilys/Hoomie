cf6b152017523e7868e9976ff2a5dd7b009e03e0
Guillaume Ferron <guillaumef.22@gmail.com>
2017-12-13 13:48:10 +0100

    New method giving the current month as a number to a literal string. Cf doc

--- src/utils/methods.js
+++ src/utils/methods.js
@@ -85,3 +85,44 @@ export function dateStrToInt(date) {
 
     return parseInt(dateInt);
 }
+
+/**
+ * Convert the current month to a litteral string. Ex. : 08th month is August
+ *
+ * @returns {string}
+ */
+export function getCurrentMonthAsStr() {
+    let monthStr = getCurrentMonth();
+    monthStr = monthStr.split("-");
+
+    let monthNb = parseInt(monthStr[1]);
+
+    switch(monthNb) {
+        case 1:
+            return "January";
+        case 2:
+            return "February";
+        case 3:
+            return "March";
+        case 4:
+            return "April";
+        case 5:
+            return "May";
+        case 6:
+            return "June";
+        case 7:
+            return "July";
+        case 8:
+            return "August";
+        case 9:
+            return "September";
+        case 10:
+            return "October";
+        case 11:
+            return "November";
+        case 12:
+            return "December";
+        default:
+            return "N/A"
+    }
+}
