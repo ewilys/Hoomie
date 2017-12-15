dc67143b621dffffae419e549dede792a8614099
Guillaume Ferron <guillaumef.22@gmail.com>
2017-12-15 13:54:17 +0100

    User Header component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import {ScrollView} from "react-native";
 import {screenStyle} from "../../utils/constants";
 import Advice from "../ui/Advice";
+import UserHeader from "../ui/UserHeader";
 
 class HomeScreen extends Component {
 
     render() {
         return (
             <ScrollView style={screenStyle}>
+                <UserHeader name="Guillaume"/>
                 <Advice content="Do you know that if you lower your heater temperature by 1°C during the night, you'll save 7% on your energy bill ?"/>
                 <Advice content="Hey ! You forgot to close your window today !"/>
             </ScrollView>
--- src/components/ui/UserHeader.js
+++ src/components/ui/UserHeader.js
@@ -1,3 +1,46 @@
 /**
  Created by Guillaume Ferron on the 12/15/2017
  **/
+
+import React, { Component } from 'react';
+import {Image, Text, View} from "react-native";
+import PropTypes from 'prop-types';
+const Dimensions = require('Dimensions');
+
+import {colors} from "../../utils/constants";
+import WeatherWidget from "./WeatherWidget";
+
+
+class UserHeader extends Component {
+    constructor(props) {
+        super(props);
+
+        this.headerStyle = {
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            height: 100,
+            alignItems: 'center',
+        };
+
+        this.headerTextStyle = {
+            fontSize: 20,
+            marginLeft: 20
+        };
+    }
+
+    render() {
+        return (
+            <View style={this.headerStyle}>
+                <Text style={this.headerTextStyle}>Hello {this.props.name ? this.props.name : ''}</Text>
+                <WeatherWidget/>
+            </View>
+        );
+    }
+}
+
+UserHeader.propTypes = {
+    name: PropTypes.string,
+
+};
+
+export default UserHeader;
